e48a76850d0553a6d3092bcb207603faa187361b
axldpir <danielpirs@googlemail.com>
2020-12-03 19:49:42 +0100

    Added test

---
+++ test_driver/app.dart
@@ -0,0 +1,9 @@
+import 'package:defaultflutterapp/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+
+  runApp(MyApp());
+}
